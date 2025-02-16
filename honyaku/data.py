23d4efab943854f9e5c91e4f238307fd9cf91146
saiteki-kai <giuseppe.magazu@gmail.com>
2025-02-17 00:13:12 +0100

    fix: load dataset from local parquet file

--- honyaku/data.py
+++ honyaku/data.py
@@ -8,12 +8,12 @@ def hf_name_to_path(model_name: str) -> str:
 
 
 def load_data(
-    dataset_name: Path | str,
+    dataset_name_or_path: Path | str,
     split: str,
     config_name: str | None = None,
 ) -> Dataset | DatasetDict | IterableDataset | IterableDatasetDict:
     """Load dataset from huggingface or local."""
-    if isinstance(dataset_name, Path):
-        return load_dataset(str(dataset_name), name=config_name, split=split)
+    if isinstance(dataset_name_or_path, Path):
+        return load_dataset("parquet", data_dir=str(dataset_name_or_path.parent), name=config_name, split=split)
 
-    return load_dataset(dataset_name, name=config_name, split=split)
+    return load_dataset(dataset_name_or_path, name=config_name, split=split)
